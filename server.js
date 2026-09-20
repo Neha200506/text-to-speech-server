35be5588661de725e7cb75daab61c7559cbd2858
Neha Redekar <neharedekar20@gmail.com>
2026-09-21 01:01:16 +0530

    Update CORS for production frontend

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "https://text-to-speech-client-pvwlzh5ii-labmentix-cloud-drive.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://text-to-speech-client-pvwlzh5ii-labmentix-cloud-drive.vercel.app",
+    ],
     credentials: true,
   })
 );
